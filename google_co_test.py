ef076cce7ff26f8f9446fb5562b0059527b08c3a
gif-git <64952924+gif-git@users.noreply.github.com>
2021-07-02 20:05:29 +0530

    Created using Colaboratory

---
+++ google_co_test.py
@@ -0,0 +1,60 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "google-co-test.ipynb",
+      "provenance": [],
+      "authorship_tag": "ABX9TyPiwzhpxG+EtWNHl3VFER14",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/gif-git/only-python/blob/master/google_co_test.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "de1etgYRmuz8",
+        "outputId": "fec392d5-f001-4a85-cd1b-6bed90d40dbc"
+      },
+      "source": [
+        "x = (\"apple\", \"banana\", \"cherry\", \"kiwi\", \"orange\")\n",
+        "for i in x:\n",
+        "  print(i)"
+      ],
+      "execution_count": 2,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "text": [
+            "apple\n",
+            "banana\n",
+            "cherry\n",
+            "kiwi\n",
+            "orange\n"
+          ],
+          "name": "stdout"
+        }
+      ]
+    }
+  ]
+}
